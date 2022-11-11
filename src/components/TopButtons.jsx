5bc4561d3e8731a09d3869e68e39d254345c6e40
mohamedraficp1 <mohamedraficp@gmail.com>
2022-11-11 12:04:51 +0530

    first commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "Mumbai",
+    },
+    {
+      id: 2,
+      title: "Delhi",
+    },
+    {
+      id: 3,
+      title: "London",
+    },
+    {
+      id: 4,
+      title: "Toronto",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+    {
+      id: 6,
+      title: "Chennai",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
